66864d41cb12aa8cb057a581ff3c5cf4a2649878
e.demidova <kat.demidoffa@gmail.com>
2021-09-03 02:43:26 +0300

    feat: optimization of adding an item and the ability to mark completed items

--- src/index.js
+++ src/index.js
@@ -2,28 +2,72 @@ import './css/reset.scss'
 import './css/styles.scss'
 
 let todoListArray = []
-let index = 0
-let todoListHTML = ''
+let doneListArray = []
 
-let todoItemHTML = (newItem) => `
+let index = 0
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]
+        var y = b[key]
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0))
+    })
+}
+
+let todoHTML = item => `
     <div class="list__item todo">
-        <i class="fas fa-square"></i>
-        <input type="text" class="list__item-text" value="${newItem.text}" readonly>
+        <i id="${item.id}" data-btn="doneItem" class="fas fa-square"></i>
+        <input type="text" class="list__item-text" value="${item.text}" readonly>
         <i class="fas fa-edit"></i>
         <i class="fas fa-trash"></i>
     </div>
-` 
+`
 
-addNewItem.onclick = function render() {
-    let textItem = document.querySelector('#addItemText').value.trim()
-    if (textItem) {
-        let newItem = {
+function todo() {
+    if (todoListArray) {
+        todoListArray = sortByKey(todoListArray, 'id')
+    }
+    let html = todoListArray.map(todoHTML).join('')
+    document.querySelector('#todoList').innerHTML = html
+}
+
+let doneHTML = item => `
+    <div class="list__item done">
+        <i data-btn="undoneItem" class="fas fa-check-square"></i>
+        <input type="text" class="list__item-text" value="${item.text}" readonly>
+    </div>
+`
+function done() {
+    if (doneListArray) {
+        doneListArray = sortByKey(doneListArray, 'id')
+    }
+    let html = doneListArray.map(doneHTML).join('')
+    document.querySelector('#doneList').innerHTML = html
+}
+
+btnAddItem.onclick = function addItem() {
+    let text = document.querySelector('#addItemText').value.trim()
+    if (text) {
+        let item = {
             id: index++,
-            text: textItem
+            text: text
         }
-        todoListArray.push(newItem)
-        todoListHTML += todoItemHTML(newItem)
-        document.querySelector('#todoList').innerHTML = todoListHTML
+        todoListArray.push(item)
+        todo()
     }
     document.querySelector('#addItemText').value = ''
 }
+
+document.addEventListener('click', event => {
+    event.preventDefault()
+    const btnType = event.target.dataset.btn
+    const id = +event.target.id
+    const item = todoListArray.find(item => item.id === id)
+
+    if (btnType === 'doneItem') {
+        todoListArray = todoListArray.filter(item => item.id !== id)
+        todo()
+        doneListArray.push(item)
+        done()
+    }
+})
