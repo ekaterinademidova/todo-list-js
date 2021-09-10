7bae85a9a22ee33385b5f3dd2cff035767b8ead6
e.demidova <kat.demidoffa@gmail.com>
2021-09-10 17:31:37 +0300

    intermediate commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import '../css/reset.scss';
 import '../css/styles.scss';
+import '../css/modal.scss';
 
 let todoListArray = [];
 let doneListArray = [];
+let archListArray = [];
 
 let index = 0;
 let statusTabs = 0;
@@ -16,6 +18,7 @@ const todoList = document.querySelector('#todoList');
 const dropdown = document.querySelector('#dropdown');
 const dropdownText = document.querySelector('#dropdownText');
 const doneList = document.querySelector('#doneList');
+const archList = document.querySelector('#archList');
 
 const sortByKey = (array, key) => 
     array.sort((a, b) => {
@@ -45,10 +48,10 @@ const addItem = () => {
 
 const todoHTML = item => `
     <div class="list__item todo">
-        <i id="${item.id}" data-btn="doneItem" class="far fa-square"></i>
+        <i onclick="doneItem(${item.id})"class="far fa-square"></i>
         <input type="text" class="list__item-text" value="${item.text}" readonly>
         <i class="fas fa-edit"></i>
-        <i class="fas fa-trash"></i>
+        <i onclick="addModal()" class="fas fa-trash"></i>
     </div>
 `;
 
@@ -62,7 +65,7 @@ const todo = () => {
 
 const doneHTML = item => `
     <div class="list__item done">
-        <i id="${item.id}" data-btn="undoneItem" class="far fa-check-square"></i>
+        <i onclick="undoneItem(${item.id})" class="far fa-check-square"></i>
         <input type="text" class="list__item-text" value="${item.text}" readonly>
     </div>
 `;
@@ -78,6 +81,21 @@ const done = () => {
     }
     let html = doneListArray.map(doneHTML).join('');
     doneList.innerHTML = html;
+};
+
+const archHTML = item => `
+    <div class="list__item arch">
+        <i class="fas fa-minus-square"></i>
+        <input type="text" class="list__item-text"  value="${item.text}" readonly>
+    </div>
+`;
+
+const arch = () => {
+    if (archListArray.length) { 
+        archListArray = sortByKey(archListArray, 'id');
+    }
+    let html = archListArray.map(archHTML).join('');
+    archList.innerHTML = html;
 };
 
 btnAddNewItem.addEventListener('click', () => {
@@ -106,22 +124,190 @@ btnShowDoneList.addEventListener('click', () => {
     }
 });
 
-document.addEventListener('click', (event) => {
-    event.preventDefault();
-    const btnType = event.target.dataset.btn;
-    const id = +event.target.id;
-    const itemTodo = todoListArray.find(item => item.id === id);
-    const itemDone = doneListArray.find(item => item.id === id);
+// $(".fa-square").click(function (id) {  
+//     const itemTodo = todoListArray.find(item => item.id === id);
+//     todoListArray = todoListArray.filter(item => item.id !== id);
+//     todo();
+//     doneListArray.push(itemTodo);
+//     done();
+// });
 
-    if (btnType === 'doneItem') {
-        todoListArray = todoListArray.filter(item => item.id !== id);
-        todo();
-        doneListArray.push(itemTodo);
-        done();
-    } else if (btnType === 'undoneItem') {
-        doneListArray = doneListArray.filter(item => item.id !== id);
-        done();
-        todoListArray.push(itemDone);
-        todo();
-    }
-});
+function doneItem(id) {
+    const itemTodo = todoListArray.find(item => item.id === id);
+    todoListArray = todoListArray.filter(item => item.id !== id);
+    todo();
+    doneListArray.push(itemTodo);
+    done();
+}
+
+function undoneItem(id) {
+    const itemDone = doneListArray.find(item => item.id === id);
+    doneListArray = doneListArray.filter(item => item.id !== id);
+    done();
+    todoListArray.push(itemDone);
+    todo();
+}
+
+function addModal() {
+    _createModal(modalRemove);
+    modal.classList.add('open');
+}
+
+function deleteModal() {
+    modal.classList.remove('open');
+    modal.classList.add('hide');
+    setTimeout(() => {
+        modal.classList.remove('hide');
+    }, 200)
+    modal.remove();
+}
+
+function deleteItem(id) {
+    const itemTodo = todoListArray.find(item => item.id === id);
+    todoListArray = todoListArray.filter(item => item.id !== id);
+    todo();
+    archListArray.push(itemTodo);
+    arch();
+    deleteModal();
+}
+
+
+
+
+
+// document.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const btnType = event.target.dataset.btn;
+//     const id = +event.target.id;
+//     const itemTodo = todoListArray.find(item => item.id === id);
+//     const itemDone = doneListArray.find(item => item.id === id);
+
+//     if (btnType === 'doneItem') {
+//         todoListArray = todoListArray.filter(item => item.id !== id);
+//         todo();
+//         doneListArray.push(itemTodo);
+//         done();
+//     } else if (btnType === 'undoneItem') {
+//         doneListArray = doneListArray.filter(item => item.id !== id);
+//         done();
+//         todoListArray.push(itemDone);
+//         todo();
+//     } 
+//     else if (btnType === 'remove') {
+//         _createModal(modalRemove);
+//         modal.classList.add('open')
+//     }
+//     if (btnType === 'remove-yes') {
+//         todoListArray = todoListArray.filter(item => item.id !== id);
+//         todo();
+//         archListArray.push(itemTodo);
+//         arch();
+        
+//         modal.classList.remove('open');
+//         modal.classList.add('hide');
+//         setTimeout(() => {
+//             modal.classList.remove('hide')
+//         }, 200)
+//         modal.remove();
+//     } else if (btnType === 'remove-no') {
+//         modal.classList.remove('open');
+//         modal.classList.add('hide');
+//         setTimeout(() => {
+//             modal.classList.remove('hide')
+//         }, 200)
+//         modal.remove();
+//     }
+    
+// });
+
+const modalRemove = {
+    title: 'Вы уверены, что хотите удалить этот элемент списка?', 
+    width: '400px',
+    content: 'gfdg',
+    footerButtons: [
+        {text: 'Отменить', type: 'secondary'},
+        {text: 'Удалить', type: 'danger'}
+    ]
+}
+
+let modal;
+
+function _createModal(options) {
+    const DEFAULT_WIDTH = '600px';
+    modal = document.createElement('div');
+    modal.classList.add('vmodal');
+    modal.innerHTML = `
+        <div class="modal-overlay"  data-close="true">
+            <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
+                <div class="modal-header">
+                    <span class="modal-title">${options.title || 'Окно'}</span>
+                    <span class="modal-close" data-close="true">&times;</span>
+                </div>
+                <div class="modal-body" data-content>
+                    ${options.content || ''}
+                </div>
+                <div class="modal-footer">
+                    <button onclick="deleteModal()" class="btn btn-${options.footerButtons[0].type || 'secondary'}">${options.footerButtons[0].text}</button>
+                    <button onclick="deleteItem(${item.id})" class="btn btn-${options.footerButtons[1].type || 'secondary'}">${options.footerButtons[1].text}</button>
+                </div>
+            </div>
+        </div>
+    `;
+    const container = document.getElementById('container');
+    document.body.insertBefore(modal, container);
+}
+
+// function _createModal() {
+//     const modal = document.createElement('div');
+//     modal.classList.add('vmodal');
+//     modal.insertAdjacentHTML('afterbegin', `
+//         <div class="modal-overlay">
+//             <div class="modal-window">
+//                 <div class="modal-header">
+//                     <div class="modal-title">Modal title</div>
+//                     <div class="modal-close">&times;</div>
+//                 </div>
+//                 <div class="modal-body">
+//                     <p>Lorem ipsum dolor sit.</p>
+//                     <p>Lorem ipsum dolor sit.</p>
+//                 </div>
+//                 <div class="modal-footer">
+//                     <button>Ok</button>
+//                     <button>Cancel</button>
+//                 </div>
+//             </div>
+//         </div>
+//     `);
+//     document.body.appendChild(modal);
+//     return modal;
+// }
+
+// const modal = _createModal();
+
+    
+        // open() {
+        //     $modal.classList.add('open');
+        // },
+        // close() {
+        //     $modal.classList.remove('open');
+        // },
+        // destroy() {
+    
+
+
+
+
+
+
+
+
+
+
+
+
+//const modal = $.modal()
+//console.log(modal);
+//modal.open()
+//modal.open()
+
+
