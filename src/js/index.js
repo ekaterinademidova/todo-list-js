effd1e22f4a3f6e4d1e6aa978926a2b0a38b8ef3
e.demidova <kat.demidoffa@gmail.com>
2021-09-20 15:33:19 +0300

    feat: split the file into several separate parts

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import '../css/reset.scss';
 import '../css/styles.scss';
 import '../css/modal.scss';
+import * as layouts from './layouts'
+import * as modals from './modals'
 //import { v4 as uuidv4 } from 'uuid';
 
 let todoListArray = [];
@@ -23,17 +25,6 @@ const dropdown = document.querySelector('#dropdown');
 const dropdownText = document.querySelector('#dropdownText');
 const doneList = document.querySelector('#doneList');
 const archList = document.querySelector('#archList');
-
-let modal;
-const modalRemove = {
-    title: 'Removing a list item', 
-    width: '400px',
-    content: 'You are about to remove a list item. It will be impossible to restore it.',
-    footerButtons: [
-        {text: 'Cancel', type: 'secondary'},
-        {text: 'Delete', type: 'danger'}
-    ]
-}
 
 const sortByKey = (array, key) => 
     array.sort((a, b) => {
@@ -101,49 +92,13 @@ const archItem = (id) => {
     todo();
     archListArray.push(itemTodo);
     arch();
-    deleteModal();
 }
-
-const addModal = (id) => {
-    _createModal(modalRemove);
-    modal.classList.add('open');
-    const cancel = document.querySelector(`#cancel`);
-    const eventCancel = () => {
-        deleteModal();
-        cancel.removeEventListener('click', eventCancel);
-    };
-    cancel.addEventListener('click', eventCancel);
-    const perform = document.querySelector(`#perform`);
-    const eventPerform = () => {
-        archItem(id);
-        perform.removeEventListener('click', eventPerform);
-    };
-    perform.addEventListener('click', eventPerform);
-}
-
-const deleteModal = () => {
-    modal.classList.remove('open');
-    modal.classList.add('hide');
-    setTimeout(() => {
-        modal.classList.remove('hide');
-    }, 200)
-    modal.remove();
-}
-
-const todoHTML = item => `
-    <div class="list__item todo">
-        <i id="todo${item.id}" class="far fa-square"></i>
-        <input type="text" class="list__item-text" value="${item.text}" readonly>
-        <i class="fas fa-edit"></i>
-        <i id="arch${item.id}" data-btn="modalDelete" class="fas fa-trash"></i>
-    </div>
-`;
 
 const todo = () => {
     if (todoListArray.length) {
         todoListArray = sortByKey(todoListArray, 'id');       
     }
-    let html = todoListArray.map(todoHTML).join('');
+    let html = todoListArray.map(layouts.todoHTML).join('');
     todoList.innerHTML = html;
     todoListArray.forEach((todo) => {
         const performTodoItem = document.querySelector(`#todo${todo.id}`);
@@ -154,18 +109,18 @@ const todo = () => {
         performTodoItem.addEventListener('click', eventPerformItem);
         const archTodoItem = document.querySelector(`#arch${todo.id}`);
         const eventArchItem = () => {
-            addModal(todo.id);
+            const modal = modals.createModal(modals.remove);
+            const perform = document.querySelector(`#perform`);
+            const eventPerform = () => {
+                archItem(todo.id);
+                modals.deleteModal(modal);
+                perform.removeEventListener('click', eventPerform);
+            };
+            perform.addEventListener('click', eventPerform);
         };
         archTodoItem.addEventListener('click', eventArchItem);
     });
 };
-
-const doneHTML = item => `
-    <div class="list__item done">
-        <i id="${item.id}" class="far fa-check-square"></i>
-        <input type="text" class="list__item-text" value="${item.text}" readonly>
-    </div>
-`;
 
 const done = () => {
     if (doneListArray.length) { 
@@ -176,7 +131,7 @@ const done = () => {
     else {
         dropdown.style.display = 'none';
     }
-    let html = doneListArray.map(doneHTML).join('');
+    let html = doneListArray.map(layouts.doneHTML).join('');
     doneList.innerHTML = html;
     doneListArray.forEach((done) => {
         const doneItem = document.querySelector(`#${done.id}`);
@@ -188,18 +143,11 @@ const done = () => {
     });
 };
 
-const archHTML = item => `
-    <div id="${item.id}" class="list__item arch">
-        <i class="far fa-minus-square"></i>
-        <input type="text" class="list__item-text" value="${item.text}" readonly>
-    </div>
-`;
-
 const arch = () => {
     if (archListArray.length) { 
         archListArray = sortByKey(archListArray, 'id');
     }
-    let html = archListArray.map(archHTML).join('');
+    let html = archListArray.map(layouts.archHTML).join('');
     archList.innerHTML = html;
 };
 
@@ -228,27 +176,3 @@ btnShowDoneList.addEventListener('click', () => {
         statusDropdown--;
     }
 });
-
-const _createModal = (options) => {
-    const DEFAULT_WIDTH = '600px';
-    modal = document.createElement('div');
-    modal.classList.add('vmodal');
-    modal.innerHTML = `
-        <div class="modal-overlay">
-            <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
-                <div class="modal-header">
-                    <span class="modal-title">${options.title || 'Окно'}</span>
-                </div>
-                <div class="modal-body">
-                    ${options.content || ''}
-                </div>
-                <div id="buttons" class="modal-footer">
-                    <button id="cancel" class="btn btn-${options.footerButtons[0].type || 'secondary'}">${options.footerButtons[0].text}</button>
-                    <button id="perform" class="btn btn-${options.footerButtons[1].type || 'secondary'}">${options.footerButtons[1].text}</button>
-                </div>
-            </div>
-        </div>
-    `;
-    const container = document.getElementById('container');
-    document.body.insertBefore(modal, container);
-}
