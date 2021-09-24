59228d04e2331a49bb703ff7248094fcd6d6b541
Ekaterina Demidova <86684392+ekaterinademidova@users.noreply.github.com>
2021-09-25 01:20:45 +0300

    feat: add event to edit item and title, add ability to change theme  (#3)
    * feat: add the required identifiers
    
    * feat: add styles to edit the item
    
    * feat: add event to edit the item
    
    * feat: add events to edit the title
    
    * feat: add styles to edit the title
    
    * feat: add the required identifiers
    
    * feat: add the required identifiers
    
    * feat: add the ability to change themes
    
    * feat: improve the readability of the code
    
    * feat: add the ability to restore an item

--- src/js/index.js
+++ src/js/index.js
@@ -12,9 +12,23 @@ let archListArray = [];
 let index = 0;
 let statusTabs = 0;
 let statusDropdown = 0;
+let statusEditing;
+let titleText;
+let selectedTheme = 'light';
 
+const themes = document.querySelector('#themes');
+const theme = document.querySelector('#theme');
+
+const addItemText = document.querySelector('#addItemText');
 const btnAddNewItem = document.querySelector('#btnAddNewItem');
+
 const btnShowDoneList = document.querySelector('#btnShowDoneList');
+
+const editTitle = document.querySelector('#editTitle');
+const cancelEditTitle = document.querySelector('#cancelEditTitle');
+const saveEditTitle = document.querySelector('#saveEditTitle');
+const title = document.querySelector('#title');
+const titleWrap = document.querySelector('#titleWrap');
 
 const tabs = document.querySelector('#tabs');
 const actuals = document.querySelector('#actuals');
@@ -26,10 +40,10 @@ const dropdownText = document.querySelector('#dropdownText');
 const doneList = document.querySelector('#doneList');
 const archList = document.querySelector('#archList');
 
-const sortByKey = (array, key) => 
+const sortByKey = (array, key) =>
     array.sort((a, b) => {
-        const x = +a[key].replace(/\D/g,'');;
-        const y = +b[key].replace(/\D/g,'');;
+        const x = +a[key].replace(/\D/g,'');
+        const y = +b[key].replace(/\D/g,'');
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
 
@@ -55,8 +69,7 @@ const addItem = () => {
         actuals.classList.add('active');
         archList.style.display = 'none';
     }
-    const item = document.querySelector('#addItemText');
-    let text = item.value.trim();
+    let text = addItemText.value.trim();
     if (text) {
         let item = {
             id: '_item_' + index++,
@@ -66,8 +79,8 @@ const addItem = () => {
         todoListArray.push(item);
         todo();
     }
-    item.value = '';
-    item.focus();
+    addItemText.value = '';
+    addItemText.focus();
 };
 
 const doneItem = (id) => {
@@ -76,7 +89,7 @@ const doneItem = (id) => {
     todo();
     doneListArray.push(itemTodo);
     done();
-}
+};
 
 const undoneItem = (id) => {
     const itemDone = doneListArray.find(item => item.id === id);
@@ -84,7 +97,7 @@ const undoneItem = (id) => {
     done();
     todoListArray.push(itemDone);
     todo();
-}
+};
 
 const archItem = (id) => {
     const itemTodo = todoListArray.find(item => item.id === id);
@@ -92,33 +105,104 @@ const archItem = (id) => {
     todo();
     archListArray.push(itemTodo);
     arch();
-}
+};
+
+const restoreItem = (id) => {
+    const itemArch = archListArray.find(item => item.id === id);
+    archListArray = archListArray.filter(item => item.id !== id);
+    arch();
+    todoListArray.push(itemArch);
+    todo();
+};
+
+const cancel = (item, itemText) => {
+    item.classList.remove('edit');
+    itemText.setAttribute('readonly', true);
+    statusEditing = false;
+    showBtn(true);
+};
+
+const showBtn = (show) => {
+    const elements = document.getElementsByClassName("list__item todo");
+    for (let i = 0; i < elements.length; i++) {
+        if (show) {
+            elements[i].classList.remove('hide');
+        } else {
+            elements[i].classList.add('hide');
+        }
+    }
+};
+
+const eventCancelEdit = (item, itemText, text) => {
+    itemText.value = text;
+    cancel(item, itemText);
+};
+
+const eventSaveEdit = (id, item, itemText) => {
+    const index = todoListArray.findIndex((item => item.id === id));
+    todoListArray[index].text = itemText.value;
+    cancel(item, itemText);
+};
 
 const todo = () => {
+    statusEditing = false;
     if (todoListArray.length) {
         todoListArray = sortByKey(todoListArray, 'id');       
     }
     let html = todoListArray.map(layouts.todoHTML).join('');
     todoList.innerHTML = html;
+
     todoListArray.forEach((todo) => {
-        const performTodoItem = document.querySelector(`#todo${todo.id}`);
+        const performTodoItem = document.querySelector(`#undone${todo.id}`);
         const eventPerformItem = () => {
             doneItem(todo.id);
             performTodoItem.removeEventListener('click', eventPerformItem);
         };
         performTodoItem.addEventListener('click', eventPerformItem);
+
         const archTodoItem = document.querySelector(`#arch${todo.id}`);
         const eventArchItem = () => {
-            const modal = modals.createModal(modals.remove);
-            const perform = document.querySelector(`#perform`);
-            const eventPerform = () => {
-                archItem(todo.id);
-                modals.deleteModal(modal);
-                perform.removeEventListener('click', eventPerform);
-            };
-            perform.addEventListener('click', eventPerform);
+            if (!statusEditing) {
+                const modal = modals.createModal(modals.remove);
+
+                const perform = document.querySelector(`#perform`);
+                const eventPerform = () => {
+                    archItem(todo.id);
+                    modals.deleteModal(modal);
+                    perform.removeEventListener('click', eventPerform);
+                };
+                perform.addEventListener('click', eventPerform);
+            }
         };
         archTodoItem.addEventListener('click', eventArchItem);
+
+        const editTodoItem = document.querySelector(`#edit${todo.id}`);
+        const eventEditItem = () => {
+            if (!statusEditing) {
+                statusEditing = true;
+                const itemText = document.querySelector(`#text${todo.id}`);
+                const text = itemText.value;
+                itemText.removeAttribute('readonly');
+                itemText.focus();
+                itemText.selectionStart = itemText.value.length;
+                const item = document.querySelector(`#${todo.id}`);
+                item.classList.add('edit');
+                showBtn(false);
+
+                const cancelEdit = document.querySelector(`#cancel${todo.id}`);
+                cancelEdit.addEventListener('click', function(){eventCancelEdit(item, itemText, text)});
+
+                const saveEdit = document.querySelector(`#save${todo.id}`);
+                saveEdit.addEventListener('click', function(){eventSaveEdit(todo.id, item, itemText)});
+
+                itemText.addEventListener('keydown', (event) => {
+                    if (event.code == 'Enter') {
+                        eventSaveEdit(todo.id, item, itemText);
+                    }
+                });
+            }
+        };
+        editTodoItem.addEventListener('click', eventEditItem);
     });
 };
 
@@ -134,7 +218,7 @@ const done = () => {
     let html = doneListArray.map(layouts.doneHTML).join('');
     doneList.innerHTML = html;
     doneListArray.forEach((done) => {
-        const doneItem = document.querySelector(`#${done.id}`);
+        const doneItem = document.querySelector(`#done${done.id}`);
         const eventCancelItem = () => {
             undoneItem(done.id);
             doneItem.removeEventListener('click', eventCancelItem);
@@ -149,6 +233,14 @@ const arch = () => {
     }
     let html = archListArray.map(layouts.archHTML).join('');
     archList.innerHTML = html;
+    archListArray.forEach((arch) => {
+        const archItem = document.querySelector(`#arch${arch.id}`);
+        const eventRestoreItem = () => {
+            restoreItem(arch.id);
+            archItem.removeEventListener('click', eventRestoreItem);
+        };
+        archItem.addEventListener('click', eventRestoreItem);
+    });
 };
 
 btnAddNewItem.addEventListener('click', () => {
@@ -156,7 +248,7 @@ btnAddNewItem.addEventListener('click', () => {
     addItem();
 });
 
-document.addEventListener('keydown', (event) => {
+addItemText.addEventListener('keydown', (event) => {
     if (event.code == 'Enter') {
         addItem();
     }
@@ -174,5 +266,57 @@ btnShowDoneList.addEventListener('click', () => {
         btnShowDoneList.classList.add('fa-chevron-right');
         doneList.style.display = 'none';
         statusDropdown--;
+    }
+});
+
+editTitle.addEventListener('click', () => {
+    titleText = title.value;
+    title.removeAttribute('readonly');
+    title.focus();
+    title.selectionStart = title.value.length;
+    titleWrap.classList.add('change');
+});
+
+const cancelEdit = () => {
+    title.value = titleText;
+    title.setAttribute('readonly', true);
+    titleWrap.classList.remove('change');
+};
+cancelEditTitle.addEventListener('click', cancelEdit);
+
+const saveEdit = () => {
+    titleText = title.value;
+    cancelEdit();
+};
+saveEditTitle.addEventListener('click', saveEdit);
+
+title.addEventListener('keydown', (event) => {
+    if (event.code == 'Enter') {
+        saveEdit();
+    }
+});
+
+themes.addEventListener('click', () => {
+    if (selectedTheme == 'light') {
+        document.documentElement.style.setProperty('--main-bg-color', '#1e1f25');
+        document.documentElement.style.setProperty('--main-text-color', '#dadada');
+        document.documentElement.style.setProperty('--main-accent-color', '#fff');
+        document.documentElement.style.setProperty('--secondary-accent-color', '#7d7d94');
+        document.documentElement.style.setProperty('--main-overlay-color', 'rgba(255, 255, 255, 0.2)');
+        document.documentElement.style.setProperty('--secondary-overlay-color', 'black');
+        theme.classList.remove('fa-moon');
+        theme.classList.add('fa-sun');
+        selectedTheme = 'dark';
+    }
+    else {
+        document.documentElement.style.setProperty('--main-bg-color', 'white');
+        document.documentElement.style.setProperty('--main-text-color', '#575767');
+        document.documentElement.style.setProperty('--main-accent-color', 'black');
+        document.documentElement.style.setProperty('--secondary-accent-color', '#ebebeb');
+        document.documentElement.style.setProperty('--main-overlay-color', 'rgba(0, 0, 0, 0.5)');
+        document.documentElement.style.setProperty('--secondary-overlay-color', 'black');
+        theme.classList.remove('fa-sun');
+        theme.classList.add('fa-moon');
+        selectedTheme = 'light';
     }
 });
--- src/js/layouts.js
+++ src/js/layouts.js
@@ -1,15 +1,18 @@
 export const todoHTML = item => `
-    <div class="list__item todo">
-        <i id="todo${item.id}" class="far fa-square"></i>
-        <input type="text" class="list__item-text" value="${item.text}" readonly>
-        <i class="fas fa-edit"></i>
-        <i id="arch${item.id}" data-btn="modalDelete" class="fas fa-trash"></i>
+    <div id="${item.id}" class="list__item todo">
+        <i id="undone${item.id}" class="far fa-square"></i>
+        <i class="fas fa-spinner"></i>
+        <input id="text${item.id}" type="text" class="list__item-text" value="${item.text}" readonly>
+        <i id="edit${item.id}" class="fas fa-edit"></i>
+        <i id="arch${item.id}" class="fas fa-trash"></i>
+        <i id="cancel${item.id}" class="fas fa-times"></i>
+        <i id="save${item.id}" class="fas fa-save"></i>
     </div>
 `;
 
 export const doneHTML = item => `
     <div class="list__item done">
-        <i id="${item.id}" class="far fa-check-square"></i>
+        <i id="done${item.id}" class="far fa-check-square"></i>
         <input type="text" class="list__item-text" value="${item.text}" readonly>
     </div>
 `;
@@ -18,5 +21,6 @@ export const archHTML = item => `
     <div id="${item.id}" class="list__item arch">
         <i class="far fa-minus-square"></i>
         <input type="text" class="list__item-text" value="${item.text}" readonly>
+        <i id="arch${item.id}" class="fas fa-undo-alt"></i>
     </div>
 `;
